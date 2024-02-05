6216efec36c9a6363ae2ecb45baa082e2a2537e0
lvy <upwardsrwr@gmail.com>
2024-02-06 00:22:11 +0800

    feat: color

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+// import { render, screen } from '@testing-library/recat'
+
+// test('renders learn react link 2', () => {
+//   const { container } = render(<App />)
+//   const linkElement = container.querySelector('.App-link')
+
+//   expect(linkElement?.textContent).toMatch(/learn react/i)
+// })
--- src/components/ColorPickerPanel/index.tsx
+++ src/components/ColorPickerPanel/index.tsx
@@ -1,20 +1,31 @@
 import { CSSProperties } from 'react'
-import cs from 'classnames'
+// import cs from 'classnames'
+// import { Color } from './color'
+import { ColorType } from './interface'
 
 export interface ColorPickerProps {
   className?: string
   style?: CSSProperties
+  value?: ColorType
+  onChange?: (color: ColorType) => void
 }
 
 function ColorPickerPanel(props: ColorPickerProps) {
-  const { className, style } = props
+  console.log(props)
+  // const { className, style, value, onChange } = props
+  // const [colorValue, setColorValue] = useState<Color>(() => {
+  //   if (value instanceof Color) {
+  //     return value
+  //   }
+  //   return new Color(value)
+  // })
 
-  const classNames = cs('color-picker', className)
+  // const classNames = cs('color-picker', className)
 
   return (
     <div
-      className={classNames}
-      style={style}
+    // className={classNames}
+    // style={style}
     ></div>
   )
 }
