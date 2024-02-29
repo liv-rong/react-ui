1aaabb49213e78ccb4f274c90b52e1008aa63c3c
lvy <upwardsrwr@gmail.com>
2024-02-29 19:49:28 +0800

    feat: store

--- src/store/my-store.ts
+++ src/store/my-store.ts
@@ -1,1 +1,69 @@
-const createStore = () => {}
+import { useEffect, useState } from 'react'
+
+const createStore = (createState: any) => {
+  //state 是全局状态
+  let state: any
+  //一个集合 用于存储订阅状态变化的监听器
+  const listeners: Set<any> = new Set()
+  //partial表示要更新的部分状态 而replace表示是否要替换整个状态。
+  const setState = (partial: any, replace: any) => {
+    const nextState = typeof partial === 'function' ? partial(state) : partial
+
+    // 比较严格相等
+    if (!Object.is(nextState, state)) {
+      const previousState = state
+
+      if (!replace) {
+        state =
+          typeof nextState !== 'object' || nextState === null
+            ? nextState
+            : Object.assign({}, state, nextState)
+      } else {
+        state = nextState
+      }
+      listeners.forEach((listener) => listener(state, previousState))
+    }
+  }
+
+  const getState = () => state
+
+  const subscribe = (listener: any) => {
+    listeners.add(listener)
+    return () => listeners.delete(listener)
+  }
+
+  const destroy = () => {
+    listeners.clear()
+  }
+
+  const api = { setState, getState, subscribe, destroy }
+
+  state = createState(setState, getState, api)
+
+  return api
+}
+
+function useStore(api: any, selector: any) {
+  const [, forceRender] = useState(0)
+  useEffect(() => {
+    api.subscribe((state: any, prevState: any) => {
+      const newObj = selector(state)
+      const oldobj = selector(prevState)
+
+      if (newObj !== oldobj) {
+        forceRender(Math.random())
+      }
+    })
+  }, [])
+  return selector(api.getState())
+}
+
+export const create = (createState: any) => {
+  const api = createStore(createState)
+
+  const useBoundStore = (selector: any) => useStore(api, selector)
+
+  Object.assign(useBoundStore, api)
+
+  return useBoundStore
+}
